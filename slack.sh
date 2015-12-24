82fbfaea20e70b4a309ee835122454534b609247
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-12-24 20:14:06 +0900

    Add simple notify

--- slack.sh
+++ slack.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+if [ -z "$SLACK_WEBHOOK_TOKEN" ]; then
+  echo "Please set the Slack webhook token to `SLACK_WEBHOOK_TOKEN`."
+  exit 1
+fi
+
+webhook_url="https://hooks.slack.com/services/${SLACK_WEBHOOK_TOKEN}"
+username="zabbix"
+channel="$1"
+subject="$2"
+color="good"
+
+payload="payload={
+  \"channel\": \"${channel}\",
+  \"username\": \"${username}\",
+  \"attachments\": [
+    {
+      \"color\": \"${color}\",
+      \"title\": \"${subject}\"
+    }
+  ]
+}"
+
+echo "[$(date)] notification to ${channel}"
+curl --data-urlencode "${payload}" "${webhook_url}"
