f06fb00c5c69fb74577102aec99321b9c66d8675
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-12-25 20:49:38 +0900

    Update message

--- slack.sh
+++ slack.sh
@@ -18,27 +18,81 @@ if [ -z "$SLACK_WEBHOOK_URL" ]; then
   exit 1
 fi
 
-function extract-tag() {
-    local message=$1
-    local tag=$2
-    echo "${message}" | awk -F "${tag}: " '{ print $2 }' | tr -d '\012'
+extract-tag() {
+  local message=$1
+  local tag=$2
+  echo "${message}" | grep "${tag}" |  awk -F "${tag}: " '{ print $2 }' | tr -d '\012'
 }
 
+triage() {
+  local severity=$1
+  local status=$1
+
+  if [ $status = 'OK' ]; then
+    color='good'
+  elif [ $status = 'PROBLEM' ]; then
+    if [ $severity = 'Information' ]; then
+      color='warning'
+    elif [ $severity = 'Warning' ]; then
+      color='warning'
+    else
+      color='danger'
+    fi
+  else
+    color=''
+  fi
+  echo $color
+}
 
 webhook_url="${SLACK_WEBHOOK_URL}"
 username="zabbix"
 channel="$1"
 subject="$2"
 message="$3"
-color="good"
+
+host=$(extract-tag "${message}" 'Host conn')
+event_id=$(extract-tag "${message}" 'Event ID')
+trigger=$(extract-tag "${message}" 'Trigger')
+trigger_id=$(extract-tag "${message}" 'Trigger ID')
+url="https://${host}/tr_events.php?trigger_id=${trigger_id}&event_id=${event_id}"
+
+status=$(extract-tag "${message}" 'Trigger status')
+severity=$(extract-tag "${message}" 'Trigger severity')
+item_name=$(extract-tag "${message}" 'Item name')
+item_value=$(extract-tag "${message}" 'Item value')
+datetime=$(extract-tag "${message}" 'Datetime')
+
+color=$(triage "${status}" "${severity}")
 
 payload="payload={
   \"channel\": \"${channel}\",
   \"username\": \"${username}\",
+  \"text\": \"<${url}|${trigger}>\",
   \"attachments\": [
     {
       \"color\": \"${color}\",
-      \"title\": \"${subject}\"
+      \"fields\": [
+        {
+          \"title\": \"Severity\",
+          \"value\": \"${severity}\",
+          \"short\": true
+        },
+        {
+          \"title\": \"Status\",
+          \"value\": \"${status}\",
+          \"short\": true
+        },
+        {
+          \"title\": \"${item_name}\",
+          \"value\": \"${item_value}\",
+          \"short\": true
+        },
+        {
+          \"title\": \"Datetime\",
+          \"value\": \"${datetime}\",
+          \"short\": true
+        }
+      ]
     }
   ]
 }"
