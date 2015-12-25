f405fa429877a7c3e16dc4a703ceb173c92b300b
Tomohiro TAIRA <tomohiro.t@gmail.com>
2015-12-25 09:55:45 +0900

    Support message argument

--- slack.sh
+++ slack.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#
+# Usage:
+#
+#   $ ./slack.sh <channel> <subject> <message>
+#
+# Example:
+#
+#   $ ./slack.sh "#alerts" "[PROBREM] unreachable httpd" "This is example"
+#
+# References:
+#
+#   Custom alertscripts document
+#   https://www.zabbix.com/documentation/2.4/manual/config/notifications/media/script
 
 if [ -z "$SLACK_WEBHOOK_TOKEN" ]; then
-  echo "Please set the Slack webhook token to `SLACK_WEBHOOK_TOKEN`."
+  echo 'Please set the Slack webhook token to `SLACK_WEBHOOK_TOKEN`.'
   exit 1
 fi
 
@@ -9,6 +22,7 @@ webhook_url="https://hooks.slack.com/services/${SLACK_WEBHOOK_TOKEN}"
 username="zabbix"
 channel="$1"
 subject="$2"
+message="$3"
 color="good"
 
 payload="payload={
